7b3afe84d7d9d530c3c1c7df9dc727bf909394ad
yangyuxin <yangyuxin0714@gmail.com>
2022-05-18 13:36:49 -0700

    msg

---
+++ index.js
@@ -0,0 +1,13 @@
+let moods = ["Falling In Love Vibe", "Sad", "Energetic", "Ready For The Day"]
+
+function generateMoods() {
+    console.log(111)
+    let parent = document.querySelector('.moodContainer');
+    // console.log(parent);
+    for (let i = 0; i < moods.length; i++) {
+        let ele = document.createElement('span');
+        ele.innerHTML = moods[i] + " | ";
+        parent.appendChild(ele);
+    }
+}
+generateMoods()
